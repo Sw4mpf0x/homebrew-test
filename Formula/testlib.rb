72346889ac55410bcf9b7d60c8780e55055e0ddf
Andrew Luke <andrew.eric.luke@gmail.com>
2025-09-03 15:49:06 -0700

    New Version

--- Formula/testlib.rb
+++ Formula/testlib.rb
@@ -1,9 +1,9 @@
 class Testlib < Formula
     desc "Test Tap Activate"
     homepage "https://github.com/Sw4mpf0x/"
-    url https://github.com/Sw4mpf0x/homebrew-test/releases/download/v2.74.3/testlib-2.74.3.tar.gz"
-    sha256 "e17f678f3df14c24e4ef9115623fdb88cec13653adef1072bdb77f5c8cf37756"
-    version "2.74.3"
+    url "https://github.com/Sw4mpf0x/homebrew-test/releases/download/v2.74.4/testlib-2.74.4.tar.gz"
+    sha256 "8f7565ad6d5137c4329d53aa762f2fed100ceb74587bf6a54784fcfb1c2943a1"
+    version "2.74.4"
 
     def install
         bin.install Dir["*"]
@@ -11,7 +11,7 @@ class Testlib < Formula
         system "codesign", "--force", "--sign", "-", f if File.file?(f)
         end
         system "python3 files/install.py"
-  end
+      end
 
     def caveats
         <<~EOS
