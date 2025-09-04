942046243c58f9b2ef637982f9794db9cf421729
Andrew Luke <andrew.eric.luke@gmail.com>
2025-09-04 10:59:09 -0700

    New Version

--- Formula/testlib.rb
+++ Formula/testlib.rb
@@ -1,9 +1,9 @@
 class Testlib < Formula
     desc "Test Tap Activate"
     homepage "https://github.com/Sw4mpf0x/"
-    url "https://github.com/Sw4mpf0x/homebrew-test/releases/download/v2.74.7/testlib-2.74.7.tar.gz"
-    sha256 "7fdf945b43a5aeaf886db3bb9dd766cc47721a457b37e873551273f6d05e7862"
-    version "2.74.7"
+    url "https://github.com/Sw4mpf0x/homebrew-test/releases/download/v2.74.8/testlib-2.74.8.tar.gz"
+    sha256 "76bc22e6f19aedd9b3d929eb7e3285681715a5eb4c1b67581546b9c41eafd0ac"
+    version "2.74.8"
 
     def install
         bin.install Dir["*"]
